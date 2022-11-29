6a39db9dd4426518e47be02daace43d9b24d7843
Narendrakoya171 <koya.narendra01@gmail.com>
2022-11-29 23:04:06 +0530

    my components commit

--- code-fiddles/src/app/app-routing.module.ts
+++ code-fiddles/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent },
+  {path:'create-account', component:CreateAccountComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
